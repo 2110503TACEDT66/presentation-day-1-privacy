ae0452d35741143816c43df4be70ce7a639a6148
NpSoHs <kuyyamasdfg@gmail.com>
2024-03-01 15:58:01 +0700

    create logout feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,6 +44,18 @@ exports.login = async(req,res,next)=>{
     }
 }
 
+exports.logout = async(req,res,next)=>{
+    res.cookie('token','none',{
+        expires: new Date(Date.now()+10*1000),
+        httpOnly:true
+    })
+
+    res.status(200).json({
+        success:true,
+        data:{}
+    })
+}
+
 exports.getMe = async(req,res,next)=>{
     try {
         if(!req.user){
