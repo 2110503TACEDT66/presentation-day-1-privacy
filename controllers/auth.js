7a5da8c7267f69e7c8d07eebe8beffc0ccf2d774
NpSoHs <kuyyamasdfg@gmail.com>
2024-03-01 16:38:08 +0700

    edit register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,17 @@
 const User = require('../models/User')
 
 exports.register = async(req,res,next)=>{
-    const {name, email, password, role, tel} = req.body;
     try {
+        const {name,email,password,role} = req.body;
+
         const user = await User.create({
-            name,
-            email,
-            password,
-            role,
-            tel
-        })
-        
-        if(!user){
-            return res.status(400).json({success:false});
-        }
-        sendTokenResponce(user,200,res)
+            name,email,password,role
+        });
+        sendTokenResponse(user,200,res)
     } catch (error) {
-        res.status(400).json({success:false,error});
+        res.status(400).json({success:false});
+        console.log(error.stack);
+        
     }
 
 }
