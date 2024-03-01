183a29c1f657b8993f8dd9a19e12d0198ef0dcac
NpSoHs <kuyyamasdfg@gmail.com>
2024-03-01 15:48:04 +0700

    add controller auth.js

---
+++ controllers/auth.js
@@ -0,0 +1,73 @@
+const User = require('../models/User')
+
+exports.register = async(req,res,next)=>{
+    const {name, email, password, role, tel} = req.body;
+    try {
+        const user = await User.create({
+            name,
+            email,
+            password,
+            role,
+            tel
+        })
+        
+        if(!user){
+            return res.status(400).json({success:false});
+        }
+        sendTokenResponce(user,200,res)
+    } catch (error) {
+        res.status(400).json({success:false,error});
+    }
+
+}
+
+
+exports.login = async(req,res,next)=>{
+    const {email,password} = req.body;
+    try {
+        if(!email || !password){
+            return res.status(400).json({success:false,msg:"please provide email and password"});
+        }
+        const user = await User.findOne({email:email}).select('+password');
+        if(!user){
+            return res.status(400).json({success:false,msg:"No User in DB"});
+        }
+        const matchPassword = await user.matchPassword(password);
+        if(!matchPassword){
+            return res.status(400).json({success:false,msg:"invalid password"});
+        }
+
+
+        sendTokenResponce(user,200,res)
+    } catch (error) {
+        res.status(400).json({success:false,error});
+    }
+}
+
+exports.getMe = async(req,res,next)=>{
+    try {
+        if(!req.user){
+            return res.status(400).json({success:false})
+        }
+        const user = await User.findById(req.user.id);
+        if(!user){
+            return res.status(400).json({success:false});
+        }
+        res.status(200).json({success:true,data:user});
+    } catch (error) {
+        res.status(400).json({success:false,error});
+    }
+}
+
+const sendTokenResponce = (user,statusCode,res)=>{
+    const token = user.getSignedJwtoken();
+    const options ={
+        expires:new Date(Date.now()+process.env.JWT_COOKIE*24*60*60*1000),
+        httpOnly:true
+    }
+    if(process.env.NODE_ENV === 'production'){
+        options.secure = true;
+    }
+    res.status(statusCode).cookie("token",token,options).json({success:true,data:user})
+}
+
