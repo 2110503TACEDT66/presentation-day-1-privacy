b598697e32cc605f0eb9b1db82bc8d3b95501080
torrak <156417791+torrak@users.noreply.github.com>
2024-03-01 15:57:37 +0700

    mount routers

--- server.js
+++ server.js
@@ -3,31 +3,36 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db')
 const cookieParser = require('cookie-parser');
-const auth = require('./routes/auth');
+
 //config dotenv
 dotenv.config({path:'./config/config.env'});
 
 //connect MONGOOSE
 connectDB();
 
+//Route files
+const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
+const appointments = require('./routes/appointments');
 
-//comment
+//Body parser
 const app = express(); 
-app.use(cookieParser());
 app.use(express.json());
 
+//Cookie parser
+app.use(cookieParser());
 
-//route
-app.use('api/v1/auth',auth);
- 
- 
-
-
+//Mount routers
+app.use('/api/v1/dentists', dentists);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/appointments', appointments);
 
 const PORT = process.env.PORT || 5050;
 const server = app.listen(PORT,console.log('Server running in ',process.env.PORT,' mode on port ',PORT));
 
+//Handle unhandled promise rejections
 process.on('unhandledRejection',(err,promise)=>{
     console.log(`Error: ${err.message}`);
+    //Close server & exit process
     server.close(()=>process.exit(1));
 });
