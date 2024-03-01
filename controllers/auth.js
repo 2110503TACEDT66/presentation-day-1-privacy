479273157b51f632bb8b891aa82ecd48c92ebff9
NpSoHs <kuyyamasdfg@gmail.com>
2024-03-01 16:41:45 +0700

    edit register

--- controllers/auth.js
+++ controllers/auth.js
@@ -66,14 +66,17 @@ exports.getMe = async(req,res,next)=>{
     }
 }
 
-const sendTokenResponce = (user,statusCode,res)=>{
-    const token = user.getSignedJwtoken();
-    const options ={
-        expires:new Date(Date.now()+process.env.JWT_COOKIE*24*60*60*1000),
+const sendTokenResponse = (user,statusCode,res)=>{
+    const token = user.getSignedJwtToken();
+    const options = {
+        expires:new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),
         httpOnly:true
     }
     if(process.env.NODE_ENV === 'production'){
         options.secure = true;
     }
-    res.status(statusCode).cookie("token",token,options).json({success:true,data:user})
+    res.status(statusCode).cookie('token',token,options).json({
+        success:true,
+        token 
+    })
 }
